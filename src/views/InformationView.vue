bec409698bbe31e5c349cdd93bd7d194dc826478
Nisha Pandey <np0091459@gmail.comm>
2023-01-13 12:51:26 +0545

    information page completed

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -8,14 +8,45 @@
             <h1>{{ information.title }}</h1>
             <p v-html="information.description"></p>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-4 py-4">
             <div class="right">
               <h4>Related Link</h4>
-              <ul>
-                <li>
-                  <a href="">{{ information.title }}</a>
+              <ul class="py-3">
+                <li
+                  v-for="(information, index) in informations"
+                  :key="index"
+                  class="card-body"
+                >
+                  <a
+                    href=""
+                    class="card"
+                    @click.prevent="
+                      $router.push({
+                        name: 'information',
+                        params: { slug: information.slug },
+                      })
+                    "
+                    >{{ information.title }}</a
+                  >
                 </li>
               </ul>
+            </div>
+            <div class="py-3">
+              <div class="right">
+                <h4>About Us</h4>
+                <ul class="py-3">
+                  <li v-for="(about, index) in about" :key="index" class="card-body">
+                    <a
+                      href=""
+                      class="card"
+                      @click.prevent="
+                        $router.push({ name: 'about', params: { slug: about.slug } })
+                      "
+                      >{{ about.title }}</a
+                    >
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -32,6 +63,7 @@ export default {
   computed: {
     ...mapGetters({
       informations: "get_information",
+      about: "get_about",
     }),
 
     information() {
@@ -59,5 +91,24 @@ h4 {
   margin-bottom: 0;
   background-color: rgba(0, 0, 0, 0.03);
   border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+.card-body {
+  padding: 5px;
+  list-style: none;
+}
+
+.card {
+  background-color: var(--primaryColor);
+  color: white;
+  text-decoration: none;
+  display: inline-block;
+  padding: 0.35em 0.65em;
+  font-size: 0.75em;
+  font-weight: 700;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+  border-radius: 0.25rem;
 }
 </style>
